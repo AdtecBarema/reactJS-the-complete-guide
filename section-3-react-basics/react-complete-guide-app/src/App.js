aa862036711bf615852639014687217b8fd05cc8
addisu mekonnen marilign <addisumekonnenmarilign@Addisus-MBP.home>
2022-12-23 23:43:55 -0600

    section-3 react basics and components

---
+++ section-3-react-basics/react-complete-guide-app/src/App.js
@@ -0,0 +1,40 @@
+import Expenses from "./components/Expenses"
+function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Grossery",
+      amount: 294.67,
+      date: new Date(2022, 8, 21),
+    },
+    {
+      id: "e2",
+      title: "Car Insurance",
+      amount: 300,
+      date: new Date(2022, 12, 21),
+    },
+    {
+      id: "e3",
+      title: "Car Gas",
+      amount: 100,
+      date: new Date(2022, 10, 14),
+    },
+    {
+      id: "e4",
+      title: "Telescope for space Observatory",
+      amount: 1000,
+      date: new Date(2022, 11, 10),
+    }
+  ];
+
+  return (
+    <div>
+    <h2 className= "">Getting started with React: </h2>
+<Expenses items={expenses}/>
+     
+    
+    </div>
+  );
+}
+
+export default App;
