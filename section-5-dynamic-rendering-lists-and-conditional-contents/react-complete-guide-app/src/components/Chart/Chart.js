5105f985a171c10d15cbe121ddddaad0cc0c2ece
addisu mekonnen marilign <addisumekonnenmarilign@Addisus-MBP.home>
2022-12-28 22:06:26 -0600

    section-5-dynamic rendering

--- section-5-dynamic-rendering-lists-and-conditional-contents/react-complete-guide-app/src/components/Chart/Chart.js
+++ section-5-dynamic-rendering-lists-and-conditional-contents/react-complete-guide-app/src/components/Chart/Chart.js
@@ -1,15 +1,15 @@
-import React from "react";
-import ChartBar from "./ChartBar";
-import "./Chart.css";
+import React from 'react';
+
+import ChartBar from './ChartBar';
+import './Chart.css';
 
 const Chart = (props) => {
-    
-  const dataPointValues = props.dataPoints.map((dataPoint) => dataPoint.value); //We are extracting only array of number from the object:dataPoints
+  const dataPointValues = props.dataPoints.map(dataPoint => dataPoint.value);
   const totalMaximum = Math.max(...dataPointValues);
 
   return (
-    <div className="chart">
-      {props.dataPoints.map((dataPoint)=> ( 
+    <div className='chart'>
+      {props.dataPoints.map((dataPoint) => (
         <ChartBar
           key={dataPoint.label}
           value={dataPoint.value}
