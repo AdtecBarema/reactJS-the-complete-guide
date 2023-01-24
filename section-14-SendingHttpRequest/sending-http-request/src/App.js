10bb8da4e28303ad955280145b292596c7b48a4c
addisu mekonnen marilign <addisumekonnenmarilign@Addisus-MBP.home>
2023-01-23 22:41:48 -0600

    Sending Http request using ASYNC-Await

--- section-14-SendingHttpRequest/sending-http-request/src/App.js
+++ section-14-SendingHttpRequest/sending-http-request/src/App.js
@@ -6,22 +6,20 @@ import "./App.css";
 function App() {
   const [movies, setMovies] = useState([]);
 
-  function fetchMoviesHandler() {
-    fetch("https://swapi.dev/api/films")
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        const transformedMovies = data.results.map((movieData) => {
-          return {
-            id: movieData.episode_id,
-            title: movieData.title,
-            openingText: movieData.opening_crawl,
-            releaseDate: movieData.release_Date,
-          };
-        });
-        setMovies(transformedMovies);
-      });
+  async function fetchMoviesHandler() {
+    const response = await fetch("https://swapi.dev/api/films");
+
+    const data = await response.json();
+
+    const transformedMovies = data.results.map((movieData) => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_Date,
+      };
+    });
+    setMovies(transformedMovies);
   }
 
   return (
